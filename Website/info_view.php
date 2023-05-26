78b654cb709598f06b6378e89a39b7ae6782c5d8
KevinKr04 <89681@glr.nl>
2023-05-26 14:13:37 +0200

    start form checks

--- Website/info_view.php
+++ Website/info_view.php
@@ -28,21 +28,21 @@
         </div>
         <div id="form">
             <h2>Doe een bod</h2>
-            <form method="post" action="handle_submit.php">
+            <form>
 
                 <label for="fname">Voornaam:</label><br>
-                <input type="text" id="fname" name="fname" required><br>
+                <input type="text" id="fname" name="fname" ><br>
                 <label for="lname">Achternaam:</label><br>
-                <input type="text" id="lname" name="lname" required><br>
+                <input type="text" id="lname" name="lname" ><br>
                 <label for="tnum">Telefoonnummer:</label><br>
-                <input type="tel" id="tnum" name="tnum" placeholder="+31 6 xxxxxxxx" pattern="+31[6]{1}[0-9]{8}" required><br>
+                <input type="tel" id="tnum" name="tnum" placeholder="+31 6 xxxxxxxx" pattern="+31[6]{1}[0-9]{8}" ><br>
                 <label for="mail">E-mail:</label><br>
-                <input type="text" id="mail" name="mail" required><br>
+                <input type="text" id="mail" name="mail" ><br>
                 <label for="bod">Bod:</label><br>
-                <input type="number" id="bod" name="bod" value="1000000" min="1000000"required><br>
+                <input type="number" id="bod" name="bod" value="1000000" min="1000000"><br>
                 <input type="hidden" name="id" value="<?= $_GET['id'] ?>">
 
-                <input type="submit" name="submit" value="Bied">
+                <input type="button" onclick="formcheck()" name="submit" value="Bied">
             </form>
         </div>
     </div>
