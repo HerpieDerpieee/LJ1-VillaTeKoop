d5ed702052630355be499857650538d89df69912
Lukaas <122445633+Lukaas123@users.noreply.github.com>
2023-05-26 15:02:01 +0200

    paar dingetjes doen ja dat

--- Website/contact_view.php
+++ Website/contact_view.php
@@ -1,3 +1,9 @@
-<div id="main">
+<head>
+    <link href="contact.css" rel="stylesheet">
+    <link href='https://fonts.googleapis.com/css?family=Roboto Condensed' rel='stylesheet'>
+</head>
 
+<div id="main">
+    <p id="titel">Neem contact met ons op</p>
+    <p></p>
 </div>
