c972c080c8b955ca738516a2db7eed40ccc64687
KevinKr04 <89681@glr.nl>
2023-05-26 15:04:51 +0200

    formcheck progresie

--- Website/info.js
+++ Website/info.js
@@ -1,3 +1,8 @@
+document.getElementById("submit").addEventListener("click", (event) => {
+    event.preventDefault()
+    formCheck()
+})
+
 let namecheck = /^[A-Za-z]*$/;
 let fnametrue = false;
 let lnametrue = false;
@@ -6,7 +11,20 @@ let bodtrue = false;
 
 function formCheck()
 {
-    if(document.getElementById(fname) == namecheck){
-        console.log("true")
+    let fname = document.getElementById("fname").value;
+    let lname = document.getElementById("lname").value;
+    let tnum = document.getElementById("tnum").value;
+    let bod = document.getElementById("bod").value;
+    if(namecheck.test(fname)){
+        fnametrue = true
+    }
+    if(namecheck.test(lname)){
+        lnametrue = true
+    }
+    if(!isNaN(tnum)){
+        tnumtrue = true
+    }
+    if(!isNaN(bod) && bod > 999999){
+        bodtrue = true
     }
 }
