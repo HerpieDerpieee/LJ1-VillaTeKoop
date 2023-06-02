ba6e8dafd6bea5112f23e4db28bb0564a01bd3a1
KevinKr04 <89681@glr.nl>
2023-06-02 14:37:41 +0200

    moved formCheck

--- Website/info.js
+++ Website/info.js
@@ -1,36 +1,3 @@
-document.getElementById("submit").addEventListener("click", (event) => {
-    event.preventDefault()
-    formCheck()
-})
-
-let namecheck = /^[A-Za-z]*$/;
-let fnametrue = false;
-let lnametrue = false;
-let tnumtrue = false;
-let bodtrue = false;
-
-function formCheck()
-{
-    let fname = document.getElementById("fname").value;
-    let lname = document.getElementById("lname").value;
-    let tnum = document.getElementById("tnum").value;
-    let bod = document.getElementById("bod").value;
-    if(namecheck.test(fname)){
-        fnametrue = true
-    }
-    if(namecheck.test(lname)){
-        lnametrue = true
-    }
-    if(!isNaN(tnum)){
-        tnumtrue = true
-    }
-    if(!isNaN(bod) && bod > 999999){
-        bodtrue = true
-    }
-}
-
-
-
 //SLIDER STUFF
 
 let slideIndex = 1;
