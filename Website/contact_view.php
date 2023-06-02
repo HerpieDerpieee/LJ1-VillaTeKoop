ca2c30c1bfe7cc2e4ee08b73bf76e675fde42874
Lukaas <122445633+Lukaas123@users.noreply.github.com>
2023-06-02 14:27:20 +0200

    contact pagina maken
    alleen nog tel num email en socials en dan alle back-end

--- Website/contact_view.php
+++ Website/contact_view.php
@@ -6,4 +6,28 @@
 <div id="main">
     <p id="titel">Neem contact met ons op</p>
     <p></p>
+    <div id="justi">
+    <form id="formu">
+        <label for="fname">Voornaam:</label>
+        <input type="text" id="fname" name="fname">
+        <label for="lname">Achternaam:</label>
+        <input type="text" id="lname" name="lname" >
+        <label for="tnum">Telefoonnummer:</label>
+        <input type="tel" id="tnum" name="tnum" placeholder="06 xxxxxxxx" pattern="[06]{1}[0-9]{8}" value="06">
+        <label for="mail">E-mail:</label>
+        <input type="text" id="mail" name="mail" >
+        <label for="vraag">Vraag/opmerking:</label>
+        <textarea type="text" id="vraag" name="vraag" rows="5"> </textarea>
+
+        <input type="button" id="verzend" name="verzend" value="verzend">
+    </form>
+    </div>
+    <div id="outer-div">
+        <div id="inner-div1">
+
+        </div>
+        <div id="inner-div2">
+
+        </div>
+    </div>
 </div>
